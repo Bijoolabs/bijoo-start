d4e7e85521e2f078874252aee8fe0b7712c61b91
Ethyde <Ethyde@hotmail.fr>
2016-05-13 15:02:10 +0200

    fix - typo

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,17 +1,17 @@
 import { sum, square, variable, MyClass } from './modules/common_module.js';
 
 // 25
-console.log(square(5));
+console.log( square(5) );
 
 var cred = {
     name: 'Ritesh Kumar',
     enrollmentNo: 11115078
 }
 
-var x = new MyClass(cred);
+var x = new MyClass( cred );
 
 //Ritesh Kumar
-console.log(x.getName()); 
+console.log( x.getName() ); 
 
 // Load jQuery has module, and use it
 
